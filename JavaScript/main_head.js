0a5db9349c48bd2a0b0d9d941e1162ec16f8d197
AC-13-13 <wang0705@126.com>
2024-06-29 12:06:47 +0800

    Add files via upload

---
+++ JavaScript/main_head.js
@@ -0,0 +1,6 @@
+var main_name="AC-13-13",main_connect=" - ";
+function begin_main_website(name)
+{
+    var out = '<title>' + name + main_connect + main_name + '</title>';
+    document.write(out);
+}
