a59aee2115652fdb9061ed855c2d8031a83753c6
AC-13-13 <wang0705@126.com>
2024-06-30 13:08:03 +0800

    Update main_head.js

--- JavaScript/main_head.js
+++ JavaScript/main_head.js
@@ -1,7 +1,6 @@
 
 function begin_main_website(name)
 {
-    var main_name="AC-13-13",main_connect=" - ";
     var out = '<title>' + name + main_connect + main_name + '</title>';
     document.write(out);
 }
