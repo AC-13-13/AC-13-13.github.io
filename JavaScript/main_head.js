90add0cab929d1d4bb2cff4689820a1edecfc923
AC-13-13 <wang0705@126.com>
2024-06-30 14:20:26 +0800

    Update main_head.js

--- JavaScript/main_head.js
+++ JavaScript/main_head.js
@@ -1,6 +1,10 @@
 
 function begin_main_website(name,main_name="AC-13-13",main_connect=" - ")
 {
-    var out = '<title>' + name + main_connect + main_name + '</title>';
+    var out = '<title>' + name + main_connect + main_name + '</title>'+
+        '<meta name="keywords" content="GitHub, github, Blog, blog, 博客, ' + main_name+ ' " > '+
+        '<meta name="description" content=' + main_name + ' \'s Blog >'+
+        '<meta name="author" content=' + main_name +' > ' +
+        '<link rel="icon"  href="../Image/icon.png">'
     document.write(out);
 }
